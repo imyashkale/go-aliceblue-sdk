8df3abf9c924ae2bb0ce1b31d24d36aaccd1d3a4
imyashkale <yashkale46@gmail.com>
2023-07-02 20:04:22 +0530

    session refreshed on 404 status code

--- service/retries.go
+++ service/retries.go
@@ -1,11 +1,17 @@
 package service
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/go-resty/resty/v2"
 )
 
 func (a *AliceBlue) retryUnAuthorized(r *resty.Response, err error) bool {
-	return r.StatusCode() == http.StatusUnauthorized
+	if r.StatusCode() == http.StatusUnauthorized {
+		a.Connect()
+		fmt.Println("request retrying due to unauthorized error")
+		return true
+	}
+	return false
 }
